b8d277a6559f840bec34cb145322397026b92968
hinerumasato <Moc Lan>
2023-11-26 21:42:21 +0700

    update query & data

---
+++ SQLQuery-Udemy.sql
@@ -0,0 +1,242 @@
+﻿use Udemy;
+SET DATEFORMAT dmy; 
+
+-----------------------VIEWS-----------------------
+
+--3.View danh sách bài học trong một khoá học: (Ngân)
+--Kết hợp thông tin từ bảng BAIHOC và KHOAHOC.
+create view v_dsbh
+as
+select BAIHOC.MABH, BAIHOC.TIEUDEBH, BAIHOC.NOIDUNGBH, BAIHOC.VIDEOBH, BAIHOC.THUTUBH, KHOAHOC.MAKH
+from BAIHOC inner join KHOAHOC on BAIHOC.MAKH = KHOAHOC.MAKH
+where KHOAHOC.MAKH = 'AI';
+
+select * from v_dsbh;
+
+--4.View danh sách đánh giá cho một khoá học: (Ngân)
+--Kết hợp thông tin từ bảng DANHGIA, HOCVIEN, và KHOAHOC.
+create view v_dsdg
+as
+select DANHGIA.MADG, DANHGIA.DIEMDG, DANHGIA.BINHLUAN, DANHGIA.NGAYDG, HOCVIEN.MAHV, KHOAHOC.MAKH
+from DANHGIA  inner join KHOAHOC on DANHGIA.MAKH = KHOAHOC.MAKH 
+			  inner join HOCVIEN on DANHGIA.MAHV = HOCVIEN.MAHV
+where KHOAHOC.MAKH = 'Figma';
+
+select * from v_dsdg;
+
+--7.View tổng điểm đánh giá (thang điểm 10) và số lượng đánh giá cho mỗi khoá học: (Bính)
+-- Tính tổng điểm và số lượng đánh giá từ bảng DANHGIA theo MAKH.
+CREATE VIEW tongdiem_soluongDanhgia
+AS
+	SELECT KHOAHOC.MAKH, AVG(DANHGIA.DIEMDG) AS TONGDIEMDG , COUNT(DANHGIA.MADG) AS SLDANHGIA
+	FROM DANHGIA INNER JOIN KHOAHOC ON DANHGIA.MAKH = KHOAHOC.MAKH
+	GROUP BY KHOAHOC.MAKH
+;
+
+SELECT * FROM tongdiem_soluongDanhgia;
+
+-----------------------PROCẺDURES-----------------------
+--3.Xóa đánh giá: (Ngân)
+--Xóa một bản ghi đánh giá từ bảng DANHGIA dựa trên MADG.
+use Udemy
+go
+
+create procedure p_xdg @madg varchar(10)
+as
+begin
+	delete from DANHGIA
+	where MADG = @madg
+end;
+
+execute p_xdg 'DG001';
+select * from DANHGIA;
+
+drop procedure p_xdg;
+
+--6.Xóa học viên: (Bính)
+ --Xóa một học viên khỏi bảng HOCVIEN và tất cả các thông tin liên quan, 
+ --chẳng hạn như đăng ký học, thanh toán, và đánh giá.
+CREATE PROC xoa_hocvien @mahv varchar(10)
+AS
+	DELETE FROM HOCVIEN WHERE HOCVIEN.MAHV = @mahv;
+	DELETE FROM DANGKYHOC WHERE DANGKYHOC.MAHV = @mahv;
+	DELETE FROM THANHTOAN WHERE THANHTOAN.MAHV = @mahv;
+	DELETE FROM DANHGIA WHERE DANHGIA.MAHV = @mahv;
+GO;
+
+EXEC xoa_hocvien 'k1101';
+SELECT * FROM HOCVIEN;
+ 	
+DROP PROC xoa_hocvien;
+
+--8.Thêm bài học mới: (Bính) 
+--	Thêm một bài học mới vào bảng BAIHOC của một khoá học cụ thể.
+CREATE PROC themBaiHocMoi @mabh varchar(10), @tieude nvarchar(255), 
+							@noidung text, @video nvarchar(255),
+							@thutu int, @makh varchar(10)
+AS
+	SET @makh = (SELECT MAKH	
+					FROM BAIHOC
+					WHERE EXISTS(SELECT MAKH
+								FROM BAIHOC)
+					)
+	IF NOT EXISTS (SELECT MABH,THUTUBH 
+					FROM BAIHOC 
+					WHERE MABH = @mabh AND @thutu IN (SELECT THUTUBH 
+														FROM BAIHOC 
+														WHERE MAKH = @makh) 
+											) 
+		BEGIN
+		INSERT INTO BAIHOC(MABH, TIEUDEBH, NOIDUNGBH, VIDEOBH, THUTUBH, MAKH)
+		VALUES (@mabh, @tieude, @noidung, @video, @thutu, @makh);
+		END
+GO;
+
+EXEC themBaiHocMoi 'AI04', 'Bài 4: Hiệu Chỉnh Vùng Nhìn, Chế Độ Hiển Thị', 'Hiệu Chỉnh','https://youtu.be/tVlZz61zS9s?si=A6bHQEG2b9XGmYpE',4,'AI';
+EXEC themBaiHocMoi 'AI05', 'Bài 4: Hiệu Chỉnh Vùng Nhìn, Chế Độ Hiển Thị', 'Hiệu Chỉnh','https://youtu.be/tVlZz61zS9s?si=A6bHQEG2b9XGmYpE',5,'AI';
+EXEC themBaiHocMoi 'AI05', 'Bài 4: Hiệu Chỉnh Vùng Nhìn, Chế Độ Hiển Thị', 'Hiệu Chỉnh','https://youtu.be/tVlZz61zS9s?si=A6bHQEG2b9XGmYpE',5,'AI';
+DROP PROC themBaiHocMoi;
+
+SELECT * FROM BAIHOC WHERE MAKH='AI';
+
+-----------------------FUNCTIONS-----------------------
+
+--2.Kiểm tra học viên đã tham gia một khoá học chưa: (Ngân)
+create function f_kthvdk(@mahv varchar(10))
+returns varchar(20)
+as
+begin
+	if exists (select HOCVIEN.MAHV
+		       from HOCVIEN inner join DANGKYHOC on HOCVIEN.MAHV = DANGKYHOC.MAHV
+						    inner join KHOAHOC on DANGKYHOC.MAKH = KHOAHOC.MAKH
+			   where HOCVIEN.MAHV in (select HOCVIEN.MAHV 
+								      from DANGKYHOC inner join HOCVIEN on DANGKYHOC.MAHV = HOCVIEN.MAHV
+								      where DANGKYHOC.MAHV = @mahv)
+			  )
+	begin 
+		return 'Da dang ky'
+	end
+	return 'Chua dang ky'
+end;
+
+print(dbo.f_kthvdk('K1103'));
+drop function f_kthvdk;
+
+--3.Tính tổng số tiền thanh toán của một học viên: (Ngân)
+create function f_tttthv(@mahv varchar(10))
+returns float
+as
+begin
+	return(
+		select SUM(THANHTOAN.SoTienTT) as Tongtienhocvienthanhtoan
+		from THANHTOAN inner join HOCVIEN on THANHTOAN.MAHV = HOCVIEN.MAHV
+		where THANHTOAN.MAHV = @mahv
+		group by HOCVIEN.MAHV
+		)
+end
+
+print(dbo.f_tttthv('K1301'));
+drop function f_tthv;
+
+--6.Lấy danh sách các học viên đã đánh giá một khoá học: (Bính)
+CREATE FUNCTION dshv_danhgiakh(@makh varchar(10))
+RETURNS TABLE
+AS
+	RETURN(
+		SELECT KHOAHOC.MAKH, HOCVIEN.* 
+		FROM HOCVIEN INNER JOIN DANGKYHOC ON HOCVIEN.MAHV = DANGKYHOC.MAHV
+			INNER JOIN KHOAHOC ON DANGKYHOC.MAKH = KHOAHOC.MAKH
+		WHERE KHOAHOC.MAKH = @makh
+	)
+;
+
+SELECT * FROM dbo.dshv_danhgiakh('AI');
+
+--8.	Lấy thông tin bài học mới nhất trong một khoá học: (Bính)
+CREATE FUNCTION f_baiHocMoiNhat(@makh varchar(10))
+RETURNS TABLE
+AS
+	RETURN(
+		SELECT *
+		FROM BAIHOC
+		WHERE BAIHOC.THUTUBH = (SELECT MAX(THUTUBH)
+									FROM BAIHOC
+									WHERE BAIHOC.MAKH = @makh
+									GROUP BY MAKH
+								)
+			AND BAIHOC.MAKH = @makh
+	)
+
+SELECT * FROM dbo.f_baiHocMoiNhat('AI');
+DROP FUNCTION f_baiHocMoiNhat;
+
+-----------------------TRIGGERS-----------------------
+
+--2.Trigger kiểm tra số lượng bài học trước khi xóa khoá học: (Ngân)
+--Trước khi xóa một khoá học khỏi bảng KHOAHOC, 
+--kiểm tra xem có bài học nào liên quan không. 
+--Nếu có, không cho phép xóa.
+create trigger delete_kh on KHOAHOC for delete
+as
+begin
+	declare @makh varchar(10)
+	set @makh = (select MAKH from deleted)
+
+	if exists (select * 
+			   from deleted
+			   where MAKH in (select MAKH from BAIHOC))
+	begin
+		raiserror('Khoa hoc co bai hoc lien quan, khong the xoa', 10,1);
+		rollback;
+	end
+	else
+		begin
+			delete from THANHTOAN where THANHTOAN.MAKH = @makh
+			delete from DANHGIA where DANHGIA.MAKH = @makh
+			delete from DANGKYHOC where DANGKYHOC.MAKH = @makh
+		end;
+end
+
+
+--4. Trigger kiểm tra trạng thái học viên trước khi xóa học viên: (Bính)
+-- Trước khi xóa một học viên khỏi bảng HOCVIEN, kiểm tra xem học viên có đang tham gia khoá học nào không. 
+-- Nếu có, không cho phép xóa.
+
+CREATE TRIGGER kt_trangthai_hocvien ON HOCVIEN instead of DELETE
+as
+begin
+	declare @mahv varchar(10);
+	set @mahv = (select mahv from deleted);
+	if exists (select DANGKYHOC.MAHV
+				from DANGKYHOC
+				where DANGKYHOC.MAHV = @mahv)
+			begin
+				RAISERROR('học viên đang đăng ký khóa học, không thể xóa', 16, 1);
+				ROLLBACK;
+			end;
+end;
+
+delete from HOCVIEN where MAHV = 'K1312';
+delete from HOCVIEN where MAHV = 'K1103';
+
+DROP TRIGGER kt_trangthai_hocvien;
+
+--CÂU TỰ TẠO: Tạo trigger kiểm tra xem khóa học mà một học viên đó đăng ký hay chưa, 
+--nếu đã đăng ký rồi thì không cho đăng ký (Bính)
+CREATE TRIGGER dangky_khoahoc ON DANGKYHOC AFTER INSERT,UPDATE
+AS
+	BEGIN
+		DECLARE @makh varchar(10), @mahv varchar(10);
+		SET @makh = (SELECT MAKH FROM inserted);
+		SET @mahv = (SELECT MAHV FROM inserted);
+		IF((select count(madkh) from dangkyhoc where mahv = @mahv and MAKH = @makh) > 1)
+		BEGIN
+			RAISERROR('học viên này đã đăng ký khóa học này', 16,1);
+			ROLLBACK;
+		END
+END;
+
+DROP TRIGGER dangky_khoahoc;
+
+INSERT INTO DANGKYHOC(MADKH, NGAYDANGKY, MAHV, MAKH)
+VALUES ('DK26', '10/10/2022', 'K1308', 'Figma');
--- Udemy.sql
+++ Udemy.sql
@@ -24,8 +24,7 @@ CREATE TABLE KHOAHOC (
 	NGAYTAO DATE NOT NULL,
 	MACD VARCHAR(10) NOT NULL,
 	MATL VARCHAR(10) NOT NULL,
-	MAGV VARCHAR(10) NOT NULL,
-	TENGV NVARCHAR (255) NOT NULL
+	MAGV VARCHAR(10) NOT NULL
 );
 --linh
 -- Cấp độ: mã cấp độ, tên cấp độ
@@ -152,22 +151,22 @@ VALUES
 ('K1312', 'Nguyen Thi Kim', 'Yen', '07/09/1986', 'Nu', '123456789', 'TP.HCM');
 
 --KHÓA HỌC
-INSERT INTO KHOAHOC(MAKH, TENKH, MOTAKH, GIA_GOC, GIA_KM, NGAYTAO, MACD, MATL, MAGV, TENGV)
+INSERT INTO KHOAHOC(MAKH, TENKH, MOTAKH, GIA_GOC, GIA_KM, NGAYTAO, MACD, MATL, MAGV)
 VALUES
 ('AI', 'Học Adobe Illustrator từ cơ bản đến nâng cao', 'Illustrator là phần mềm thiết kế đồ họa chuyên vẽ. Sử dụng các thuật toán, các đối tượng hình học, text kết hợp. Việc kết hợp giữa các hình dạng cơ bản đó với nhau để tạo thành một đối tượng vector'
-, 700000, 1200000,'26/04/2022', 'CD03', 'MATL07','GV03','Nguyễn Quang Duy'),
+, 700000, 1200000,'26/04/2022', 'CD03', 'MATL07','GV03'),
 ('DP', 'Khóa học Digital Paiting cơ bản cho người mới bắt đầu', 'Digital Painting hay còn được gọi là vẽ kỹ thuật số. Đây là một kĩ thuật vẽ mà trong đó hoạ sĩ sẽ sử dụng công nghệ '
-, 500000, 1000000, '26/04/2022', 'CD01', 'MATL05','GV02','Nguyễn Mai Phương'),
+, 500000, 1000000, '26/04/2022', 'CD01', 'MATL05','GV02'),
 ('Figma', 'Nhập môn thiết kế UX/UI, UX-UI Design', 'UX/UI Designer là người đảm nhận vai trò thiết kế trải nghiệm người dùng (User Experience Design) và thiết kế giao diện người dùng (User Interface Design)'
-, 600000, 1000000, '26/04/2022', 'CD01', 'MATL07','GV01','Huỳnh Ngọc Thanh'),
+, 600000, 1000000, '26/04/2022', 'CD01', 'MATL07','GV01'),
 ('Lightroom', 'Khóa học Lightroom - Blend màu và Retouch - Histogram là gì?', 'Lightroom là phần mềm xử lý ảnh cực kỳ mạnh mẽ và là người anh em với phần mềm Adobe Photoshop'
-, 300000, 600000, '26/04/2022', 'CD01', 'MATL08','GV05','Nguyên Bính'),
+, 300000, 600000, '26/04/2022', 'CD01', 'MATL08','GV05'),
 ('NA', 'Nhiếp ảnh cơ bản cho người mới bắt đầu', 'Nhiếp ảnh là quá trình tạo ra hình ảnh bằng tác động của ánh sáng với phim hoặc thiết bị nhạy sáng'
-, 800000, 1600000, '26/04/2022', 'CD02', 'MATL08','GV04','Tô Phương Linh'),
+, 800000, 1600000, '26/04/2022', 'CD02', 'MATL08','GV04'),
 ('PTS', 'Khóa học giúp bạn làm chủ Photoshop từ A - Z', 'Adobe Photoshop có thể được xem là phần mềm xử lý ảnh nổi tiếng nhất trên thị trường'
-, 1200000, 2000000, '26/04/2022', 'CD03', 'MATL07','GV06','Trần Thắng Lợi'),
+, 1200000, 2000000, '26/04/2022', 'CD03', 'MATL07','GV06'),
 ('AE', 'Học kỹ xảo trong sản xuất video bằng Adobe After Effects', 'Adobe After Effects là phần mềm đồ họa chuyển động số và đồ họa tổng hợp, được phát triển bởi hãng Adobe Systems.'
-, 1000000, 1500000, '26/04/2022', 'CD03', 'MATL05','GV07','Nguyễn Việt Toàn');
+, 1000000, 1500000, '26/04/2022', 'CD03', 'MATL05','GV07');
 
 -- CẤP ĐỘ
 INSERT INTO CAPDO(MACD, TENCD)
@@ -259,9 +258,9 @@ VALUES
 INSERT INTO DANGKYHOC(MADKH, NGAYDANGKY, MAHV, MAKH)
 VALUES
 ('DK01', '24/9/2023', 'K1101', 'AI'),
-('DK02', '20/11/2023', 'K1101', 'AI'),
+('DK02', '20/11/2023', 'K1101', 'DP'),
 ('DK03', '15/12/2022', 'K1102', 'DP'),
-('DK04', '10/10/2022', 'K1103', 'Figma'),
+('DK04', '10/10/2022', 'K1102', 'Figma'),
 ('DK05', '5/12/2021', 'K1103', 'Figma'),
 ('DK06', '2/11/2023', 'K1303', 'Figma'),
 ('DK07', '12/11/2022', 'K1304', 'Lightroom'),
@@ -282,162 +281,48 @@ VALUES
 --ĐÁNH GIÁ
 INSERT INTO DANHGIA(MADG, DIEMDG, BINHLUAN, NGAYDG, MAHV, MAKH)
 VALUES
-('DG001', 4.5, 'Khoa hoc bo ich', '12/12/2020', 'K1305', 'AI'),
-('DG002', 5.0, 'Rat huu ich', '15/12/2020', 'K1204', 'AI'),
-('DG003', 1.5, 'Qua te', '10/10/2020', 'K1303', 'DP'),
-('DG004', 2.5, 'Tam duoc', '11/12/2020', 'K1105', 'NA'),
-('DG005', 3.5, 'Kha on', '12/11/2020', 'K1106', 'Figma'),
-('DG006', 1.0, 'That vong cuc ky', '13/10/2020', 'K1207', 'AE'),
-('DG007', 2.0, 'te', '15/10/2020', 'K1208', 'PTS'),
-('DG008', 3.0, 'Kha bo ich', '15/11/2020', 'K1301', 'Lightroom'),
-('DG009', 4.0, 'Huu ich', '15/12/2020', 'K1109', 'NA'),
-('DG0010', 5.0, 'Tuyet voi', '15/12/2020', 'K1203', 'Figma');
-
+('DG001', 4.5, 'Khóa học bổ ích', '12/12/2022', 'K1101', 'AI'),
+('DG002', 5.0, 'Rất hữu ích', '15/12/2022', 'K1102', 'DP'),
+('DG003', 1.5, 'Quá tệ', '10/10/2022', 'K1103', 'Figma'),
+('DG004', 2.5, 'Tạm được', '11/12/2022', 'K1304', 'Lightroom'),
+('DG005', 3.5, 'Khá ổn', '12/11/2022', 'K1201', 'PTS'),
+('DG006', 1.0, 'Thất vọng cực kỳ', '13/10/2022', 'K1106', 'AE'),
+('DG007', 2.0, 'Tệ', '15/10/2022', 'K1310', 'AE'),
+('DG008', 3.0, 'Khá bổ ích', '15/11/2020', 'K1311', 'AI'),
+('DG009', 3.0, 'Ổn', '15/12/2022', 'K1211', 'AI'),
+('DG010', 4.0, 'Hữu ích', '15/12/2022', 'K1204', 'DP'),
+('DG011', 4.5, 'Khóa học bổ ích', '15/12/2022', 'K1209', 'NA'),
+('DG012', 4.2, 'Hữu ích', '15/12/2022', 'K1203', 'NA'),
+('DG013', 3.8, 'Khá hữu ích', '15/12/2022', 'K1302', 'PTS'),
+('DG014', 2.3, 'Tạm', '15/12/2022', 'K1301', 'Lightroom'),
+('DG015', 4.0, 'Hữu ích', '15/12/2022', 'K1306', 'PTS'),
+('DG016', 5.0, 'Rất hữu ích', '15/12/2022', 'K1301', 'Lightroom'),
+('DG017', 5.0, 'Tuyệt vời', '15/12/2022', 'K1203', 'DP');
 
 --THANH TOÁN
 INSERT INTO THANHTOAN (MATT, SOTIENTT, NGAYTT, TRANGTHAI, MAHV, MAKH)
 VALUES 
-('TT001', 700000, '4/11/2020', 'PAID', 'K1306', 'AE'),
-('TT002', 300000, '4/11/2020', 'NOT PAID', 'K1206', 'DP'),
-('TT003', 500000, '5/11/2020', 'NOT PAID', 'K1208', 'PTS'),
-('TT004', 1000000, '3/11/2020', 'PAID', 'K1305', 'AI'),
-('TT005', 700000, '2/11/2020', 'PAID', 'K1204', 'AI'),
-('TT006', 500000, '5/11/2020', 'PAID', 'K1105', 'NA'),
-('TT007', 900000, '4/11/2020', 'NOT PAID', 'K1301', 'Lightroom'),
-('TT008', 700000, '3/11/2020', 'PAID', 'K1203', 'Figma'),
-('TT009', 500000, '2/11/2020', 'PAID', 'K1106', 'Figma'),
-('TT0010', 300000, '4/11/2020', 'NOT PAID', 'K1109', 'NA');
+('TT001', 1200000, '24/9/2023', 'PAID', 'K1101', 'AI'),
+('TT002', 1200000, '20/11/2023', 'NOT PAID', 'K1101', 'DP'),
+('TT003', 1000000, '15/12/2022', 'NOT PAID', 'K1102', 'DP'),
+('TT004', 1000000, '10/10/2022', 'PAID', 'K1102', 'Figma'),
+('TT005', 1000000, '5/12/2021', 'PAID', 'K1103', 'Figma'),
+('TT006', 1000000, '2/11/2023', 'PAID', 'K1303', 'Figma'),
+('TT007', 600000, '12/11/2022', 'NOT PAID', 'K1304', 'Lightroom'),
+('TT008', 2000000, '24/04/2023', 'PAID', 'K1201', 'PTS'),
+('TT009', 1500000, '15/07/2022', 'PAID', 'K1106', 'AE'),
+('TT010', 1500000, '18/12/2022', 'PAID', 'K1310', 'AE'),
+('TT011', 1200000, '20/9/2023', 'PAID', 'K1311', 'AI'),
+('TT012', 1200000, '11/11/2023', 'NOT PAID', 'K1211', 'AI'),
+('TT013', 1000000, '8/12/2022', 'PAID', 'K1204', 'DP'),
+('TT014', 1600000, '3/07/2022', 'PAID', 'K1209', 'NA'),
+('TT015', 1600000, '5/1/2021', 'NOT PAID', 'K1203', 'NA'),
+('TT016', 2000000, '2/1/2023', 'PAID', 'K1203', 'PTS'),
+('TT017', 600000, '2/11/2022', 'NOT PAID', 'K1302', 'Lightroom'),
+('TT018', 2000000, '4/08/2023', 'PAID', 'K1301', 'PTS'),
+('TT019', 600000, '11/08/2022', 'PAID', 'K1306', 'Lightroom'),
+('TT020', 1000000, '17/07/2022', 'PAID', 'K1301', 'DP');
 
---VIEWS
---7.View tổng điểm đánh giá (thang điểm 10) và số lượng đánh giá cho mỗi khoá học: (bính)
--- Tính tổng điểm và số lượng đánh giá từ bảng DANHGIA theo MAKH.
-CREATE VIEW tongdiem_soluongDanhgia
-AS
-	SELECT KHOAHOC.MAKH, AVG(DANHGIA.DIEMDG) AS TONGDIEMDG , COUNT(DANHGIA.MADG) AS SLDANHGIA
-	FROM DANHGIA INNER JOIN KHOAHOC ON DANHGIA.MAKH = KHOAHOC.MAKH
-	GROUP BY KHOAHOC.MAKH
-;
 
-SELECT * FROM tongdiem_soluongDanhgia;
 
---PROCẺDURES
---6.Xóa học viên: (bính)
- --Xóa một học viên khỏi bảng HOCVIEN và tất cả các thông tin liên quan, 
- --chẳng hạn như đăng ký học, thanh toán, và đánh giá.
-CREATE PROC xoa_hocvien @mahv varchar(10)
-AS
-	DELETE FROM HOCVIEN WHERE HOCVIEN.MAHV = @mahv;
-	DELETE FROM DANGKYHOC WHERE DANGKYHOC.MAHV = @mahv;
-	DELETE FROM THANHTOAN WHERE THANHTOAN.MAHV = @mahv;
-	DELETE FROM DANHGIA WHERE DANHGIA.MAHV = @mahv;
-GO;
-
-EXEC xoa_hocvien 'k1101';
- 	
-DROP PROC xoa_hocvien;
-
---8.Thêm bài học mới: (bính) 
---	Thêm một bài học mới vào bảng BAIHOC của một khoá học cụ thể.
-CREATE PROC themBaiHocMoi @mabh varchar(10), @tieude nvarchar(255), 
-							@noidung text, @video nvarchar(255),
-							@thutu int, @makh varchar(10)
-AS
-	SET @makh = (SELECT MAKH	
-					FROM BAIHOC
-					WHERE EXISTS(SELECT MAKH
-								FROM BAIHOC)
-					)
-	IF NOT EXISTS (SELECT MABH,THUTUBH 
-					FROM BAIHOC 
-					WHERE MABH = @mabh AND @thutu IN (SELECT THUTUBH 
-														FROM BAIHOC 
-														WHERE MAKH = @makh) 
-											) 
-		BEGIN
-		INSERT INTO BAIHOC(MABH, TIEUDEBH, NOIDUNGBH, VIDEOBH, THUTUBH, MAKH)
-		VALUES (@mabh, @tieude, @noidung, @video, @thutu, @makh);
-		END
-GO;
-
-EXEC themBaiHocMoi 'AI04', 'Bài 4: Hiệu Chỉnh Vùng Nhìn, Chế Độ Hiển Thị', 'Hiệu Chỉnh','https://youtu.be/tVlZz61zS9s?si=A6bHQEG2b9XGmYpE',4,'AI';
-EXEC themBaiHocMoi 'AI05', 'Bài 4: Hiệu Chỉnh Vùng Nhìn, Chế Độ Hiển Thị', 'Hiệu Chỉnh','https://youtu.be/tVlZz61zS9s?si=A6bHQEG2b9XGmYpE',5,'AI';
-EXEC themBaiHocMoi 'AI05', 'Bài 4: Hiệu Chỉnh Vùng Nhìn, Chế Độ Hiển Thị', 'Hiệu Chỉnh','https://youtu.be/tVlZz61zS9s?si=A6bHQEG2b9XGmYpE',5,'AI';
-DROP PROC themBaiHocMoi;
-
-SELECT * FROM BAIHOC WHERE MAKH='AI';
---FUNCTIONS
---6.Lấy danh sách các học viên đã đánh giá một khoá học: (bính)
-CREATE FUNCTION dshv_danhgiakh(@makh varchar(10))
-RETURNS TABLE
-AS
-	RETURN(
-		SELECT KHOAHOC.MAKH, HOCVIEN.* 
-		FROM HOCVIEN INNER JOIN DANGKYHOC ON HOCVIEN.MAHV = DANGKYHOC.MAHV
-			INNER JOIN KHOAHOC ON DANGKYHOC.MAKH = KHOAHOC.MAKH
-		WHERE KHOAHOC.MAKH = @makh
-	)
-;
-
-SELECT * FROM dbo.dshv_danhgiakh('AI');
-
---8.	Lấy thông tin bài học mới nhất trong một khoá học: (bính)
-CREATE FUNCTION f_baiHocMoiNhat(@makh varchar(10))
-RETURNS TABLE
-AS
-	RETURN(
-		SELECT *
-		FROM BAIHOC
-		WHERE BAIHOC.THUTUBH = (SELECT MAX(THUTUBH)
-									FROM BAIHOC
-									WHERE BAIHOC.MAKH = @makh
-									GROUP BY MAKH
-								)
-			AND BAIHOC.MAKH = @makh
-	)
-
-SELECT * FROM dbo.f_baiHocMoiNhat('AI');
-DROP FUNCTION f_baiHocMoiNhat;
-
---TRIGGERS
---4. Trigger kiểm tra trạng thái học viên trước khi xóa học viên: (bính)
--- Trước khi xóa một học viên khỏi bảng HOCVIEN, kiểm tra xem học viên có đang tham gia khoá học nào không. 
--- Nếu có, không cho phép xóa.
-
-CREATE TRIGGER kt_trangthai_hocvien ON HOCVIEN instead of DELETE
-as
-begin
-	declare @mahv varchar(10);
-	set @mahv = (select mahv from deleted);
-	if exists (select DANGKYHOC.MAHV
-				from DANGKYHOC
-				where DANGKYHOC.MAHV = @mahv)
-			begin
-				RAISERROR('học viên đang đăng ký khóa học, không thể xóa', 16, 1);
-				ROLLBACK;
-			end;
-end;
-
-delete from HOCVIEN where MAHV = 'K1312';
-
-DROP TRIGGER kt_trangthai_hocvien;
-
---CÂU TỰ TẠO: Tạo trigger kiểm tra xem khóa học mà một học viên đó đăng ký hay chưa, 
---nếu đã đăng ký rồi thì không cho đăng ký
-CREATE TRIGGER dangky_khoahoc ON DANGKYHOC AFTER INSERT,UPDATE
-AS
-	BEGIN
-		DECLARE @makh varchar(10), @mahv varchar(10);
-		SET @makh = (SELECT MAKH FROM inserted);
-		SET @mahv = (SELECT MAHV FROM inserted);
-		IF((select count(madkh) from dangkyhoc where mahv = @mahv and MAKH = @makh) > 1)
-		BEGIN
-			RAISERROR('học viên này đã đăng ký khóa học này', 16,1);
-			ROLLBACK;
-		END
-END;
-
-DROP TRIGGER dangky_khoahoc;
-
-INSERT INTO DANGKYHOC(MADKH, NGAYDANGKY, MAHV, MAKH)
-VALUES ('DK26', '10/10/2022', 'K1308', 'Figma');
 
